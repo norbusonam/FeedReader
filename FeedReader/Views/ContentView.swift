095ca3f7a0234de1c04ed771e3556c31225d2fac
Norbu Sonam <43245807+norbusonam@users.noreply.github.com>
2024-02-29 22:21:49 -0800

    clean up

--- FeedReader/Views/ContentView.swift
+++ FeedReader/Views/ContentView.swift
@@ -10,11 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationSplitView {
-            List(model.employees, selection: $employeeIds) { employee in
-                Text(employee.name)
-            }
         } detail: {
-            EmployeeDetails(for: employeeIds)
         }
     }
 }
