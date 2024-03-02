9af680a9a7df197c0afcea9a112d43f3fa7e4914
Norbu Sonam <43245807+norbusonam@users.noreply.github.com>
2024-03-01 19:53:47 -0800

    delete feed functionality

--- FeedReader/Views/FeedsView.swift
+++ FeedReader/Views/FeedsView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 import SwiftData
 
 struct FeedsView: View {
+    @Environment(\.modelContext) private var modelContext
+    
     @State private var showNewFeedSheet = false
+    
     @Query private var rssFeeds: [RSSFeed]
     
     var body: some View {
@@ -20,7 +23,13 @@ struct FeedsView: View {
                 } label: {
                     Text(rssFeed.feedLink)
                 }
+                .swipeActions {
+                    Button("Delete", systemImage: "trash", role: .destructive) {
+                        modelContext.delete(rssFeed)
+                    }
+                }
             }
+            .animation(.default, value: rssFeeds.count)
             .navigationTitle("Feeds")
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
