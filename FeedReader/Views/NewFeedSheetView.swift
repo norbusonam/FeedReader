a636f5c26aa2d210c7c1465c65ad45783f05232d
Norbu Sonam <43245807+norbusonam@users.noreply.github.com>
2024-03-01 19:59:08 -0800

    focus on text field on appear

--- FeedReader/Views/NewFeedSheetView.swift
+++ FeedReader/Views/NewFeedSheetView.swift
@@ -10,11 +10,18 @@ import SwiftUI
 struct NewFeedSheetView: View {
     @Environment(\.modelContext) private var modelContext
     @Environment(\.dismiss) private var dismiss
+    
     @State private var rssFeed = RssFeed(feedLink: "")
+    
+    @FocusState private var feedLinkFocused
     
     var body: some View {
         TextField("Link to RSS Feed", text: $rssFeed.feedLink)
             .keyboardType(.URL)
+            .focused($feedLinkFocused)
+            .onAppear {
+                feedLinkFocused = true
+            }
         Button("add") {
             // TODO: validate feed
             modelContext.insert(rssFeed)
